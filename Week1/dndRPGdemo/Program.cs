f5ce69a3b5076615ec128dff81b584588e75606d
jdelacruz96 <jonathan.delacruz@revature.com>
2024-04-02 14:04:19 -0400

    added TODOs

--- Week1/dndRPGdemo/Program.cs
+++ Week1/dndRPGdemo/Program.cs
@@ -4,9 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        //TODO: Create list to store characters
+
+        //TODO: Add more hardcoded characters, append them to list
         Character johnSmith = new("John Smith", "Fighter", 24, 15);
 
+        //We user Console.Writeline to write output to the console
         Console.WriteLine(johnSmith.ToString());
+
+        //TODO: Create Switch Menu inside while loop w/user input
+
+        //TODO: Validate User Input with try-catch
 
 
     }
