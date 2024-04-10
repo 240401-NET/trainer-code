c4c6ec171ffb46df1d6fc9e3bb1a43b17467d088
jnipr <minseon.song@revature.com>
2024-04-10 10:54:49 -0400

    chinook sql server script

---
+++ Week2/joins_setops.sql
@@ -0,0 +1,5 @@
+-- Joins
+-- "Denormalizing" tables or "Joining" multiple tables *over a common column* (usually PK/FK pair)
+-- syntax: {table1} JOIN {table2} ON table1.column = table2.column
+-- Inner Join, Left/Right Join, Outer/Full Join....
+
