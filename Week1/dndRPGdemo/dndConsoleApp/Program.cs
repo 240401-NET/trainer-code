dfa4ca205058770413f79efa37366f903c4c2f0a
Soulem <ewilcher@fullsail.edu>
2024-04-12 15:12:27 -0400

    Revert "Updated Having Claus"
    This reverts commit c1703ec74bdf6b106ae056c0dc9e5d3b0469c1bf.

--- Week1/dndRPGdemo/dndConsoleApp/Program.cs
+++ Week1/dndRPGdemo/dndConsoleApp/Program.cs
@@ -4,84 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        // /* I can make a block comment like this.
-        // Everything between the two slash+asterisks will be a comment
-        // This way I can easily do multi-line comments */
+        /* I can make a block comment like this.
+        Everything between the two slash+asterisks will be a comment
+        This way I can easily do multi-line comments */
 
-        // //Create list to store characters
-        // List<Character> characterList = new();
+        //Create list to store characters
+        List<Character> characterList = new();
 
-        // Data.LoadCharacters(ref characterList);
+        Data.LoadCharacters(ref characterList);
 
-        // //Add more hardcoded characters, append them to list
-        // // Character johnSmith = new("John Smith", "Fighter", 24, 15);
-        // // Character django = new("Django", "Rogue", 27, 12);
-        // // Character shrek = new("Shrek", "Ogre", 32, 44);
+        //Add more hardcoded characters, append them to list
+        // Character johnSmith = new("John Smith", "Fighter", 24, 15);
+        // Character django = new("Django", "Rogue", 27, 12);
+        // Character shrek = new("Shrek", "Ogre", 32, 44);
 
-        // //Appending characters to the list 
-        // // characterList.Add(shrek);
-        // // characterList.Add(django);
-        // // characterList.Add(johnSmith);
+        //Appending characters to the list 
+        // characterList.Add(shrek);
+        // characterList.Add(django);
+        // characterList.Add(johnSmith);
 
-        // //We user Console.Writeline to write output to the console
-        // //Console.WriteLine(johnSmith.ToString());
-        // int userInput = 0;
+        //We user Console.Writeline to write output to the console
+        //Console.WriteLine(johnSmith.ToString());
+        int userInput = 0;
 
-        // // Create Switch Menu inside while loop w/user input
-        // while (userInput != 9)
-        // {
-        //     //Displaying the menu
-        //     Menu.PrintMenu();
+        // Create Switch Menu inside while loop w/user input
+        while (userInput != 9)
+        {
+            //Displaying the menu
+            Menu.PrintMenu();
 
-        //     //Store that input into userInput - See Logic.cs
-        //     userInput = Menu.UserChoice();
+            //Store that input into userInput - See Logic.cs
+            userInput = Menu.UserChoice();
 
-        //     //Switch upon the user input
-        //     switch (userInput)
-        //     {   
-        //         //Execute code based on whatever case we enter
-        //         case 1: //Add a character
-        //         //Created by Kung Lo
-        //         characterList.Add(Logic.GenerateUserCharacter());
-        //         break;
+            //Switch upon the user input
+            switch (userInput)
+            {   
+                //Execute code based on whatever case we enter
+                case 1: //Add a character
+                //Created by Kung Lo
+                characterList.Add(Logic.GenerateUserCharacter());
+                break;
                 
-        //         case 2: //Display all characters
-        //         Logic.DisplayCharacters(characterList);
-        //         break;
+                case 2: //Display all characters
+                Logic.DisplayCharacters(characterList);
+                break;
 
-        //         //If user enters 9 we exit the switch and the program ends
-        //         case 9:
-        //         Console.WriteLine("Goodbye!");    
-        //         break;
+                //If user enters 9 we exit the switch and the program ends
+                case 9:
+                Console.WriteLine("Goodbye!");    
+                break;
 
-        //         default:
-        //         Console.WriteLine("Invalid choice, please enter again!");
-        //         break;
-        //     }
+                default:
+                Console.WriteLine("Invalid choice, please enter again!");
+                break;
+            }
 
-        // }
+        }
 
 
-        // // Save the character list to file
-        // Data.PersistCharacters(characterList);
-        
-        foo foo1 = new();
-        foo foo2 = new();
-        foo foo3 = new();
-        foo foo4 = new();
+        // Save the character list to file
+        Data.PersistCharacters(characterList);
 
-        Console.WriteLine($"foo1 i value = {foo.i}");
-        Console.WriteLine($"foo2 i value = {foo.i}");
-        Console.WriteLine($"foo1 j value = {foo1.j}");
-        Console.WriteLine($"foo2 j value = {foo2.j}");
 
-        foo.MultiplyToProduct(3.4f);
-        foo.MultiplyToProduct(10.0f);
-        foo.MultiplyToProduct(0.0241f);
-
-        Console.WriteLine($"foo1 product value = {foo.product}");
-        
-     }
+    }
 
 
 
